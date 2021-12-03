2dab996823f631de5aec8f63fdb5ab31163f0f7d
Scott Mitchell <scott_j_mitchell@hotmail.co.uk>
2021-12-03 16:40:59 +0000

    Initial commit

---
+++ lib/3.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Submarine
+  def initialize
+  
+  end  
+end
+
---
+++ test/3_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/3'
+
+class SubmarineTest < Minitest::Test
+  def test_a
+    
+  end
+end
